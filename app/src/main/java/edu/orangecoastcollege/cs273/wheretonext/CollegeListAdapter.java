2de6af24611e4b6fd640ccc5e7e92bd7c8b0f93d
Severiano Badajoz <sevebadajoz@gmail.com>
2017-10-24 10:32:22 -0700

    Inflate the view to the list

--- app/src/main/java/edu/orangecoastcollege/cs273/wheretonext/CollegeListAdapter.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/wheretonext/CollegeListAdapter.java
@@ -2,11 +2,19 @@ package edu.orangecoastcollege.cs273.wheretonext;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -54,9 +62,21 @@ public class CollegeListAdapter extends ArrayAdapter<College> {
 
         // TODO:  Write the code to correctly inflate the view (college_list_item) with
         // TODO:  all widgets filled with the appropriate College information.
+        College selectedCollege = mCollegesList.get(pos);
+        ((TextView) view.findViewById(R.id.collegeListNameTextView)).setText(selectedCollege.getName());
+        ((RatingBar) view.findViewById(R.id.collegeListRatingBar)).setRating(selectedCollege.getRating());
 
-
-
+        AssetManager am = mContext.getAssets();
+        try {
+            InputStream stream = am.open(selectedCollege.getImageName());
+            Drawable event = Drawable.createFromStream(stream, selectedCollege.getName());
+            ((ImageView)view.findViewById(R.id.collegeListImageView)).setImageDrawable(event);
+        }
+        catch (IOException ex)
+        {
+            Log.e("Where To Next", "Couldn't load image: " + selectedCollege.getImageName());
+        }
+        view.findViewById(R.id.collegeListLinearLayout).setTag(selectedCollege);
         return view;
     }
 }
